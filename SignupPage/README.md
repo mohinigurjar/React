0a80d79f126d23033a7d96efb926d755b83ef917
Bhoopendra Singh <bhoopendrasingh2306@gmail.com>
2025-07-14 03:18:35 +0530

    updated

---
+++ SignupPage/README.md
@@ -0,0 +1,5 @@
+#Sign Up Page
+
+<br>
+It is a basic signup page created with react.<br>
+UseState, UseEffect and conditional rendering are focused in this mini project.
---
+++ SignupPage/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import SignIn from './pages/SignIn'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <SignIn/>
+      </>
+  )
+}
+
+export default App
